b1f276d98d7ac7affb51241b089b202c331089a4
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2023-01-12 20:18:11 +0100

    Implement loginUser controller function; Check password and generate token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -49,9 +49,24 @@ const registerUser = asyncHandler(async (req, res) => {
 // @desc    Authenticate a user
 // @route   POST /api/users/login
 // @access  Public
-const loginUser = (req, res) => {
-  res.json({ message: 'Login User' });
-};
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check for user email
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid credentials');
+  }
+});
 
 // @desc    Get user data
 // @route   GET /api/users/me
