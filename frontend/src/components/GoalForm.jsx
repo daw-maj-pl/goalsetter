c29980dc5c08e524756f70014c341604426f9084
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2023-01-30 19:33:03 +0100

    Dispatch createGoal action creator

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createGoal } from '../features/goals/goalSlice';
 
 function GoalForm() {
   const [text, setText] = useState('');
 
+  const dispatch = useDispatch();
+
   const onSubmit = e => {
     e.preventDefault();
 
+    dispatch(createGoal({ text }));
     setText('');
   };
 
