106412be423a2fd56973960d37cac05dec072c73
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2023-02-02 20:00:18 +0100

    Implement GoalItem component

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,5 +1,11 @@
-function GoalItem() {
-  return <div>GoalItem</div>;
+function GoalItem({ goal }) {
+  return (
+    <div className="goal">
+      <div>{new Date(goal.createdAt).toLocaleString()}</div>
+      <h2>{goal.text}</h2>
+      <button className="close">X</button>
+    </div>
+  );
 }
 
 export default GoalItem;
